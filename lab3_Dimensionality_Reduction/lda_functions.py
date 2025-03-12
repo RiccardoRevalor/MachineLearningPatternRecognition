fb79adfb9bc7ffa14f1ed2c771e223c93163c57e
RiccardoRevalor <riccardorevalor@gmail.com>
2025-03-12 22:56:22 +0100

    l03 LDA method2: Joint Diagonalization of matrices Sb, Sw to solve the gen. eigvalue problem

--- lab3_Dimensionality_Reduction/lda_functions.py
+++ lab3_Dimensionality_Reduction/lda_functions.py
@@ -98,6 +98,7 @@ def computeSwSb(D, L):
 
 
 
+# FIRST METHOD 
 def calculateLDA(D, L, m):
     '''
     Params:
@@ -113,11 +114,11 @@ def calculateLDA(D, L, m):
     Sb, Sw = computeSwSb(D, L)
 
     #2) then, solve the generalized Eigenvalues problem
-    #matrix a is Sb -> I wanna find Sb eigenvaues
+    #matrix a is Sb -> I wanna find the eigvalues of (Sw)^{-1} * Sb 
     #matrix b is Sw which is positive definite
 
-    #sigma -> w ndarray of eigenvalues of matrix a
-    #U -> v ndarray of corresponding eigenvectors of matrix a -> they are sorted from lowest to highest as in the numpy counterpart!
+    #sigma -> w ndarray of eigenvalues of inverse of matrix b * matrix a
+    #U -> v ndarray of corresponding eigenvectors of inverse of matrix b * matrix a -> they are sorted from lowest to highest as in the numpy counterpart!
     sigma, U = sc.linalg.eigh(a=Sb, b=Sw)
 
     #I don't mind about sigma since I want the LDA directions which are the eigenvectors of U
@@ -127,6 +128,52 @@ def calculateLDA(D, L, m):
     return W
 
 
+# SECOND METHOD
+def calculateLDAbyJointDiagonalization(D, L, m):
+    '''
+    Params:
+    - D: Dataset features matrix, not ceCntered
+    - L: Labels of the samples
+    - m: number of discriminant directions of the target subplane
+
+    Returned Values:
+    - W: LDA matrix containing the most discriminant directions, calculated solving teh generalized Eigenvalue problem by joint diagonalization of Sw and Sb
+    '''
+
+    #0) first, compute S between, S within matrices as before
+    Sb, Sw = computeSwSb(D, L)
+
+    #Joint diagonaization:
+
+    #Step 1: Whiten Sw
+    #Compute SVD on matrix Sw to find U and sigma (Vh is useless)
+    U, sigma, Vh = np.linalg.svd(Sw)
+
+    #U = matrix of singular vectors of Sw
+    #sigma = matrix of singular values of Sw
+
+    #Compute P1
+    #sigma needs to be to the power of -1/2 first, then use dot products: 
+    #a. compute U @ sigma^{1/2}
+    #b. compute (U @ sigma^{1/2}) @ U.T
+    P1 = U @ np.diag(1.0/(sigma**0.5)) @ U.T
+
+    #Compute Sbt = Sbt = P1 @ Sb @P1 -> Sbt is the transformed between class covariance
+    Sbt = P1 @ Sb @ P1.T
+
+    #Compute P2, which is the matrixof eigenvectors of Sbt corresponding to its m highest eigenvalues -> so use SVD again
+    U2, sigma2, Vh2 = np.linalg.svd(Sbt) #with SVD the eigvectors are already ordered from the highest to the lowest
+
+    #just take the leading m eigvectors
+    P2 = U2[:, 0:m]
+
+
+
+    #Step 2: Find matrix W as W = P1.T @ P2
+    return P1.T @ P2
+
+
+#THIS (THE FINAL PROJECTION STEP) IS EXACTLY THE SAME FOR BOTH METHODS
 def applyLDA(D, W):
     '''
     Params:
@@ -187,3 +234,77 @@ def scatterPlots2(x, y, numFeatures):
 
     plt.tight_layout(pad=3) #add padding between subplots to distance between eachother
     plt.show()
+
+
+def densityPlots(x, y, numFeatures, title=None):
+    #I select feature i and feature j, with i different from j and plot them on the 2 axis of every scatter plot chart
+
+    #rows, cols for making the subplot
+    cols = 2
+    rows = 1
+
+    labelColors = {0: "red", 1: "green", 2: "blue"}
+    classLabels = {0: 'Iris-setosa', 1: 'Iris-versicolor', 2: 'Iris-virginica'}
+    featuresNames = {0: 'Sepal length', 1: 'Sepal width', 2: 'Petal length', 3: 'Petal width'}
+
+    #subplot creation
+    fig, plots = plt.subplots(nrows=rows, ncols=cols, figsize=(cols*6, rows*4))
+    plots = plots.flatten()   #the 2D axes array becomes a 1D array in order to access each ax in a more straighforward way during the loop
+
+    subplotCounter = 0
+    for i in range(numFeatures):
+
+        subplot = plots[subplotCounter]
+        subplotCounter+=1
+        for label in labelColors:
+            sample_with_that_class = x[i][ y == label]
+            subplot.hist(x=sample_with_that_class, color=labelColors[label], alpha= 0.7, density=True, label=f"{classLabels[label]}", edgecolor="black")
+            subplot.legend()
+            subplot.set_title(f"Feature {featuresNames[i]} Distribution")
+            subplot.set_xlabel(featuresNames[i])
+            subplot.set_ylabel("Density")
+
+    plt.tight_layout(pad=3) #add padding between subplots to distance between eachother
+    if title:
+        fig.suptitle(title, fontsize=16)  # General title
+    plt.show()
+
+
+if __name__ == "__main__":
+    D, L = loadIrisDataset(irisPath)
+
+    m = 2   #number of most discriminant directions (at max equal to C-1)
+
+    #Method 1, Standard solution to the generalized Eigvalue problem
+    W_2_Method1 = calculateLDA(D, L, m)
+
+    #Apply Method 1
+    DP_2_Method1 = applyLDA(D, W_2_Method1)
+
+    
+    
+
+
+    #Method 2 - Joint Diagonalization of Sb, Sw
+    W_2_JointDiag = calculateLDAbyJointDiagonalization(D, L, m)
+
+    #Apply Method 2
+    DP_2_JointDiag = applyLDA(D, W_2_JointDiag)
+
+    
+    
+    
+    
+    #Plot both results
+
+    scatterPlots2(DP_2_Method1, L, DP_2_Method1.shape[0])   #Method 1
+    
+    scatterPlots2(DP_2_JointDiag, L, DP_2_JointDiag.shape[0])   #Joint Diag
+    
+
+ 
+
+
+
+
+
